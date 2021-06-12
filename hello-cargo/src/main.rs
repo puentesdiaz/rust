d3804c6cbdb3e1167649bba35c31e506ee8be811
puentesdiaz <puentesdiaz@gmail.com>
2021-06-11 23:10:49 -0300

    test5

--- hello-cargo/src/main.rs
+++ hello-cargo/src/main.rs
@@ -5,15 +5,14 @@ fn main() {
 
     println!("The number is {}.", a_number);
     println!("The boolean is {}.", a_boolean);
-
+    /////////////////////////////////////////////////////////////////
     println!(
         "1 + 2 = {} and 8 - 5 = {} and 15 * 3 = {}",
         1i32 + 2,
         8i32 - 5,
         15 * 3
     );
-    
-
+    ///////////////////////////////////////////////////////////////////
     println!("9 / 2 = {} but 9.0 / 2.0 = {}", 9u32 / 2, 9.0 / 2.0);
 
     // Declare variable to store result of "greater than" test, Is 1 > 4? -- false
